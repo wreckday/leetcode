a6c286f622af27ce6041c9e8cd847b382455cb81
mellon <Mellon@Hsiang-Tings-MacBook-Pro.local>
2015-02-10 21:43:10 -0500

    modified sayHello

--- src/SayHello.java
+++ src/SayHello.java
@@ -5,7 +5,8 @@ public class SayHello {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("say hello");
+		System.out.println("say hello222222");
+		System.out.println("try it");
 
 	}
 
